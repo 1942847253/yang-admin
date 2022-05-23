d8095508f1912492c7ced9ce25a138e62c3260be
1942847253 <1942847253@qq.com>
2022-05-23 10:26:27 +0800

    fix:文件名自动格式化导致的路由路径错误

--- .eslintrc.ts
+++ .eslintrc.ts
@@ -11,10 +11,7 @@ module.exports = {
     },
 
     extends: [
-        'plugin:vue/vue3-recommended',
-        'plugin:@typescript-eslint/recommended',
-        'prettier',
-        'plugin:prettier/recommended'
+
     ],
 
     rules: {

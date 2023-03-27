3dc12ad040ea8660c56e6354ebc1b54863a1ec90
1942847253 <1942847253@qq.com>
2023-03-27 21:12:29 +0800

    test:

--- yangAdmin/src/main.ts
+++ yangAdmin/src/main.ts
@@ -2,6 +2,7 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import store from './store'
 import router from './router/index'
+import AmuUI from '../amu-ui/es'
 
 import 'element-plus/theme-chalk/dark/css-vars.css'
 import './assets/css/index.css'
@@ -12,11 +13,12 @@ import 'element-plus/theme-chalk/el-message.css';
 
 // 创建vue实例
 const app = createApp(App)
+console.log('test');
 
 for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
     app.component(key, component);
 }
-
+app.use(AmuUI);
 app.use(store);
 app.use(router);
 // 挂载实例
